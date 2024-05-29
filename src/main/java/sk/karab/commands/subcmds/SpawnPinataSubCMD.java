99e5ff6f904ad100d6b0c28c132afc4b66252362
FilipeeX <filipeexbusinessemail@gmail.com>
2024-05-29 10:02:47 +0200

    Added a command interface for spawning pinatas

--- src/main/java/sk/karab/commands/subcmds/SpawnPinataSubCMD.java
+++ src/main/java/sk/karab/commands/subcmds/SpawnPinataSubCMD.java
@@ -4,6 +4,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import sk.karab.commands.ISubCommand;
 import sk.karab.database.locations.LocationDatabase;
+import sk.karab.database.locations.PinataLocation;
+import sk.karab.messaging.Message;
+import sk.karab.messaging.Messaging;
+import sk.karab.messaging.Replacement;
 import sk.karab.pinata.Pinata;
 import sk.karab.util.PlayerUtil;
 
@@ -21,17 +25,49 @@ public class SpawnPinataSubCMD implements ISubCommand {
     @Override
     public void execute(CommandSender sender, String[] args) {
 
+        switch (args.length) {
+            case 1:
+                spawnPinataAtPlayer(sender);
+                break;
+            case 2:
+                spawnPinataAtLocation(sender, args);
+                break;
+            default:
+                Messaging.send(Message.HELP, sender);
+                break;
+        }
+    }
+
+
+    private void spawnPinataAtPlayer(CommandSender sender) {
+
         if (PlayerUtil.isNotPlayer(sender)) {
-            // todo implement locations and allow console to spawn pinata at a location
+            Messaging.send(Message.PLAYER_ONLY, sender);
             return;
         }
-
         Player player = PlayerUtil.asPlayer(sender);
         assert player != null;
 
         new Pinata(player.getLocation());
 
-        // todo message
+        Messaging.send(Message.SPAWN_PLAYER_SUCCESS, player);
+    }
+
+
+    private void spawnPinataAtLocation(CommandSender sender, String[] args) {
+
+        String locationIdentifier = args[1];
+        PinataLocation pinataLocation = LocationDatabase.getLocation(locationIdentifier);
+
+        if (pinataLocation == null) {
+            Messaging.send(Message.INVALID_LOCATION, sender,
+                    new Replacement("%identifier", locationIdentifier));
+            return;
+        }
+
+        new Pinata(pinataLocation.location());
+        Messaging.send(Message.SPAWN_LOCATION_SUCCESS, sender,
+                new Replacement("%identifier", locationIdentifier));
     }
 
 
